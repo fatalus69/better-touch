cdfef46bfbd544b58b376c34548c7ca009109e50
fatalus69 <der_fatal_@outlook.com>
2025-06-08 09:40:47 +0200

    feat: tweak build script & restructuring

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Open TODOs:
+
+- [] Options for command
+- [] Figure out how to build both windows and linux from one machine
+- [] Write windows install script
--- build.sh
+++ build.sh
@@ -5,15 +5,35 @@ set -e
 
 # Build binary of Odin
 odin build src -out=better-touch
+# odin build src -out:better-touch.exe -target:windows_amd64
 
 # Check if build is for release
 if [[ "$*" == "--release" ]]; then
-    VERSION="v1.0.0"
-    ARCHIVE_NAME="better-touch-linux-amd64-${VERSION}.tar"
-    echo "Creating release archive: $ARCHIVE_NAME"
-    # Package files
-    tar -cf "$ARCHIVE_NAME" \
-        ./better-touch \
-        ./install.sh \
-        ./man/better-touch.1
+    VERSION="v1.1.0"
+    install_files=("windows-x64" "linux-amd64")
+
+    for type in "${install_files[@]}"; do 
+        ARCHIVE_NAME="better-touch-${type}-${VERSION}"
+        echo "Creating release archive: $ARCHIVE_NAME"
+
+        if [[ $type == *"linux"* ]]; then
+            # Build man page
+            gzip -k ./man/better-touch.1
+
+            # Build archive for Linux
+            tar -cf "$ARCHIVE_NAME.tar" \
+                ./better-touch \
+                ./installers/install.sh \
+                ./man/better-touch.1.gz
+        
+            # remove built man page
+            rm ./man/better-touch.1.gz
+        else
+            # Build archive for windows
+            zip "$ARCHIVE_NAME.zip" \
+                ./better-touch.exe \
+                ./installers/install.ps1 \
+
+        fi
+    done
 fi
